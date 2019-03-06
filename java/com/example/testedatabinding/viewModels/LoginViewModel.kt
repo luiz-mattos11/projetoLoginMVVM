a4aca2db3ce7f1debd377be825fa9b08a17ce8ba
Luiz Mattos <luiz.matos@supera.com.br>
2019-03-06 10:44:20 -0300

    Criada tela de Login

--- java/com/example/testedatabinding/viewModels/LoginViewModel.kt
+++ java/com/example/testedatabinding/viewModels/LoginViewModel.kt
@@ -1,16 +1,31 @@
 package com.example.testedatabinding.viewModels
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import android.util.Log
 import android.view.View
 import com.example.testedatabinding.models.UserData
 
-class LoginViewModel() {
-    var str = ObservableField<String>()
+class LoginViewModel: ViewModel() {
+    var loginInfo:String = ""
+    var passwdInfo:String = ""
+
+    val credent:MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
     var user:UserData = UserData("","")
 
-    fun onLoginClick(view : View){
-        str.set("Logged")
+
+    fun onLoginClick(v : View){
+        user.setUser(loginInfo)
+        user.setPass(passwdInfo)
+
+        if(user.verifyCredentials()){
+            credent.value = "Logado com sucesso"
+        } else credent.value = "Usuario ou senha incorretos"
     }
 
 }
